b3ed0d3bebf2b16dc7d8cfdb4348680d8c17e08b
medProgAyat <hosseinmehriyari1383@gmail.com>
2026-05-14 08:44:51 +0330

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import pygame
+import sys
+
+pygame.init()
+width , height = 800 , 600
+screen = pygame.display.set_mode((width,height))
+pygame.display.set_caption("بازی ایران من")
+
+# ------- COLORS -------------
+
+# رنگ‌ها را تعریف می‌کنیم (RGB)
+WHITE = (255, 255, 255)
+RED   = (255, 0, 0)
+
+# ------end:COLORS------------
